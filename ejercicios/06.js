2ca10e58ee773d6774c223081270337a67ad8492
gabriela <gabyfernandez.0607@gmail.com>
2024-10-14 17:13:31 -0300

    bucles

---
+++ ejercicios/06.js
@@ -0,0 +1,8 @@
+function tieneTresDigitos(num) {
+  // Si el número recibido tiene tres dígitos, retorna true.
+  // Caso contrario, retorna false.
+  // Tu código:
+  return num >99 && num <1000
+}
+
+module.exports = tieneTresDigitos;
---
+++ ejercicios/20.js
@@ -0,0 +1,14 @@
+function sumarHastaNConBreak(n) {
+   // La función recibe un numero n por argumento.
+   // Devuelve la suma de todos los números desde 1 hasta n.
+   // Si la suma supera a 100, detén el bucle usando break.
+   // Tu código:
+   var acum = 0
+  for (var i = 1; i <= n; i++){
+   if(acum > 100) break
+    acum = acum + i
+  }
+  return acum 
+}
+
+module.exports = sumarHastaNConBreak;
